3a9720819db237d4e4f131d05b9f6d0412d3f8f9
Denise Fontanier <deniseden.c@gmail.com>
2023-12-15 18:12:06 +0100

    Etape 2 fini

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,28 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+//clic flèche gauche et flèche droite
+let arrows = document.querySelectorAll(".arrow img")
+
+for (let i=0; i<arrows.length; i++) {
+	let listArrow = arrows[i];
+	console.log(listArrow);
+
+	listArrow.addEventListener("click", (event) => {
+		myArrow = event.target
+		console.log(`cliquable : ${myArrow.className}`)
+	})
+}
+
+
+//carrousel d'images
+/*let numero = 0;
+
+function changeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slides.length - 1;
+    if (numero > slides.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slides[numero];
+}*/
